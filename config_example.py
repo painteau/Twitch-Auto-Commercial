553b233a38e8b1a3e928e7bf524ea5a263462c00
KaO <52933590+painteau@users.noreply.github.com>
2021-02-08 12:17:41 +0100

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -4,3 +4,9 @@ oauth_token = "MY CLIENT TOKEN"
 channel_name= "TWITCH CHANNEL NAME"
 difftrigger = 10 # if difference between 2 image <= difftriger, launch commercial
 rtmp = "MY RTMP FEED"
+
+# Setting the points for cropped image 
+left = 0
+top = 0
+right = 1920
+bottom = 1080
